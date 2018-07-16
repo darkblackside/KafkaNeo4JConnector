7b43a218786053eec808091f9d41c03e44a3b53a
darkblackside <bmerschmeier@aol.de>
2018-07-16 11:54:18 +0200

    Added more logging

--- src/main/java/de/dortmund/skbmtp/KafkaNeo4JConnector/logic/TransactionWorker.java
+++ src/main/java/de/dortmund/skbmtp/KafkaNeo4JConnector/logic/TransactionWorker.java
@@ -1,5 +1,7 @@
 package de.dortmund.skbmtp.KafkaNeo4JConnector.logic;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.neo4j.driver.v1.Record;
@@ -45,7 +47,13 @@ public class TransactionWorker implements TransactionWork<String>
 
 			try
 			{
+				List<String> keys = record.keys();
 				LOGGER.debug("Keys" + mapper.writeValueAsString(record.keys()));
+
+				for(String key : keys)
+				{
+					LOGGER.debug("Value of " + key + ": " + mapper.writeValueAsString(record.get(key)));
+				}
 			}
 			catch (JsonProcessingException e1)
 			{
